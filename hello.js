409e48f872c3156ab59075e9cfe00f3927351f6c
CandyQueen77 <angiepolitte@gmail.com>
2024-04-11 10:11:27 -0500

    Hello World changes

--- hello.js
+++ hello.js
@@ -4,7 +4,10 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Howdy, World!" + " Hello to you");
+
+console.log('"Nevermore"');
+
 
 /*  
 Next, PLAY! 
